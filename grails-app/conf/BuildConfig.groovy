1fb75c8f7c333f50415df731e0447f94c8314a7c
Benoit Hediard <ben@agorapulse.com>
2013-09-23 14:29:59 +0200

    Grails upgraded to 2.2.4

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,18 @@
+grails.project.work.dir = 'target'
+grails.project.source.level = 1.6
+
+grails.project.dependency.resolution = {
+    inherits 'global'
+    log 'warn'
+    repositories {
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+    }
+    dependencies {
+
+    }
+    plugins {
+        compile ':rest:0.7'
+    }
+}
