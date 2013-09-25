cd019ad51596ae456c8d9fd762a3da7d665ea215
Benoit Hediard <ben@agorapulse.com>
2013-09-25 12:25:07 +0200

    Version bumped to 2.0.0-SNAPSHOT

--- RecurlyGrailsPlugin.groovy
+++ RecurlyGrailsPlugin.groovy
@@ -2,15 +2,18 @@ import grails.plugin.recurly.RecurlyWebHookController
 
 class RecurlyGrailsPlugin {
     // the plugin version
-    def version = "0.99"
+    def version = "2.0.0-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.5 > *"
     // the other plugins this plugin depends on
     def dependsOn = ["rest": "0.6.1 > *"]
+
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
-            "grails-app/controllers/recurlyplugin/UtilController.groovy"
+            "web-app/css/**/*.*",
+            "web-app/js/**/*.*",
+            "docs/*.*"
     ]
 
     def author = "Kushal Likhi"
@@ -19,9 +22,14 @@ class RecurlyGrailsPlugin {
     def description = '''\\
 Recurly Grails API.
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://www.devunited.org/recurly-plugin/grails-recurly-plugin-usage-guide/"
+    def license = "APACHE"
+    def developers = [
+            [ name: "Kushal Likhi", email: "kushal.likhi@gmail.com" ],
+            [ name: "Benoit Hediard", email: "ben@benorama.com" ]
+    ]
+    def documentation = "https://github.com/agorapulse/grails-recurly"
+    def issueManagement = [ system: "github", url: "https://github.com/agorapulse/grails-recurly/issues" ]
+    def scm = [  url: "https://github.com/agorapulse/grails-recurly" ]
 
     def doWithApplicationContext = { applicationContext ->
         application.serviceClasses.each {service ->
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -13,6 +13,10 @@ grails.project.dependency.resolution = {
 
     }
     plugins {
+        build ':release:2.2.1', ':rest-client-builder:1.0.3', {
+            export = false
+        }
+
         compile ':rest:0.7'
     }
 }
