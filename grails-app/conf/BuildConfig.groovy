0b5df5146b0738692bbd50f4e2f7fda3cba6965b
Benoit Hediard <ben@agorapulse.com>
2013-11-09 15:11:28 +0100

    Grails upgraded to 2.3.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,26 +1,33 @@
 grails.project.work.dir = 'target'
 grails.project.source.level = 1.6
 
-grails.project.dependency.resolution = {
-    inherits 'global'
-    log 'warn'
-    repositories {
-        grailsCentral()
-        mavenLocal()
-        mavenCentral()
-    }
-    dependencies {
-    }
-    plugins {
-        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
-            export = false
+grails {
+    project {
+        dependency {
+            distribution = {
+                remoteRepository(id: "internal", url: "file://${basedir}/mvn-repo")
+            }
+            resolution = {
+                inherits 'global'
+                log 'warn'
+                repositories {
+                    grailsCentral()
+                    mavenLocal()
+                    mavenCentral()
+                }
+                dependencies {
+                }
+                plugins {
+                    build ':release:3.0.1', ':rest-client-builder:1.0.3', {
+                        export = false
+                    }
+
+                    compile ':rest:0.8'
+                }
+
+            }
+
         }
 
-        compile ':rest:0.8'
-
-        // To debug
-        compile(':console:1.2') {
-            export = false
-        }
     }
 }
