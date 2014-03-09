5803e0e87beed3949af34438bf4f65d53957cf30
Benoit Hediard <ben@agorapulse.com>
2014-03-09 21:04:43 +0100

    Grails upgraded to 2.3.6 + cleaning

--- RecurlyGrailsPlugin.groovy
+++ RecurlyGrailsPlugin.groovy
@@ -1,32 +1,21 @@
 import grails.plugin.recurly.RecurlyWebHookController
 
 class RecurlyGrailsPlugin {
-    // the plugin version
-    def version = "2.0.0"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = ["rest": "0.6.1 > *"]
 
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp",
-            "web-app/css/**/*.*",
-            "web-app/js/**/*.*",
-            "docs/*.*"
-    ]
+    def version = "2.0.0"
+    def grailsVersion = "2.0 > *"
+    def dependsOn = ["rest": "0.6.1 > *"]
 
     def author = "Kushal Likhi"
     def authorEmail = "kushal.likhi@gmail.com"
     def title = "Recurly Plugin"
-    def description = '''\\
-Recurly Grails API.
-'''
+    def description = 'Recurly Grails API.'
     def license = "APACHE"
     def developers = [
             [ name: "Kushal Likhi", email: "kushal.likhi@gmail.com" ],
             [ name: "Benoit Hediard", email: "ben@benorama.com" ]
     ]
+
     def documentation = "https://github.com/agorapulse/grails-recurly"
     def issueManagement = [ system: "github", url: "https://github.com/agorapulse/grails-recurly/issues" ]
     def scm = [  url: "https://github.com/agorapulse/grails-recurly" ]
