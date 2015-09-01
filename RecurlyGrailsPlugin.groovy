944ee89fd2053b2c7fbe1ed08c0cd924beff73a8
Florian Ernoult <floernoult@gmail.com>
2015-09-01 15:30:15 +0200

    Version bumped to 2.2.9 + README update

--- RecurlyGrailsPlugin.groovy
+++ RecurlyGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import grails.plugin.recurly.RecurlyWebHookController
 
 class RecurlyGrailsPlugin {
 
-    def version = "2.2.8"
+    def version = "2.2.9"
     def grailsVersion = "2.0 > *"
     def dependsOn = ["rest": "0.6.1 > *"]
 
