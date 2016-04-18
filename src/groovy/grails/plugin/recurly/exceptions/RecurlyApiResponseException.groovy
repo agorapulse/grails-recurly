58ab8b137ac4fe58a558659873ab26398c8fdb1b
Florian Ernoult <floernoult@gmail.com>
2016-04-18 15:38:10 +0200

    [Fix] API errors handling fixed to get full server reply

--- src/groovy/grails/plugin/recurly/exceptions/RecurlyApiResponseException.groovy
+++ src/groovy/grails/plugin/recurly/exceptions/RecurlyApiResponseException.groovy
@@ -5,6 +5,7 @@ import grails.plugin.recurly.templates.Response
 class RecurlyApiResponseException extends Exception {
 
     public String entityType
+    public Map errors
     public String message
     public String serverReply
     public int statusCode
@@ -13,11 +14,12 @@ class RecurlyApiResponseException extends Exception {
         super(response.message)
         this.entityType = response.entity.class.toString()
         this.statusCode = response.status.toInteger()
-        this.serverReply = response.errors['SERVER_REPLY'].error ? response.errors['SERVER_REPLY'].error.toString() : response.errors['SERVER_REPLY']
+        this.errors = response.errors
+        this.serverReply = (response.errors['SERVER_REPLY'].error && response.errors['SERVER_REPLY'].error.toString() != '') ? response.errors['SERVER_REPLY'].error.toString() : response.errors['SERVER_REPLY']
     }
 
     public String toString(){
-        return "RecurlyApiResponseException - statusCode: ${statusCode}, message: ${message}, entityType: ${entityType}, serverReply: ${serverReply}"
+        return "RecurlyApiResponseException - statusCode: ${statusCode}, message: ${message}, entityType: ${entityType}, serverReply: ${serverReply}, errors: ${errors}"
     }
 
 }
