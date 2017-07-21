1bfe6460a0d8d71acdf42e04cc3277fa7633d0bb
Benoit Hediard <ben@agorapulse.com>
2017-07-21 11:40:51 +0200

    Required API headers added

--- grails-app/services/grails/plugin/recurly/RESTService.groovy
+++ grails-app/services/grails/plugin/recurly/RESTService.groovy
@@ -7,16 +7,30 @@ import groovyx.net.http.RESTClient
 
 class RESTService {
 
-    static transactional = false
+    static API_VERSION = '2.7'
 
-    public Response<HttpResponseDecorator> getResponseUsingMethodPOST(String url, String data, String apiKey, String contentType = "application/xml; charset=utf-8", String acceptType = "application/xml") {
+    public Response<HttpResponseDecorator> getResponseUsingMethodPOST(String url,
+                                                                      String data,
+                                                                      String apiKey,
+                                                                      String apiVersion = API_VERSION,
+                                                                      String contentType = "application/xml; charset=utf-8",
+                                                                      String acceptType = "application/xml") {
 
         Response<HttpResponseDecorator> httpResponse = new Response<HttpResponseDecorator>()
         String authenticationDigest = apiKey.bytes.encodeBase64().toString()
 
         try {
             new RESTClient(url).with {
-                httpResponse.entity = post(body: data, contentType: contentType, headers: ["Accept": acceptType, "Authorization": authenticationDigest])
+                httpResponse.entity = post(
+                        body: data,
+                        contentType: contentType,
+                        headers: [
+                                "Accept": acceptType,
+                                "Authorization": "Basic ${authenticationDigest}",
+                                "Content-Type": contentType,
+                                "X-Api-Version": apiVersion
+                        ]
+                )
                 httpResponse.message = "Response Generated via URL: ${url}"
                 httpResponse.status = httpResponse.entity.status
             }
@@ -29,14 +43,28 @@ class RESTService {
         return httpResponse
     }
 
-    public Response<HttpResponseDecorator> getResponseUsingMethodPUT(String url, String data, String apiKey, String contentType = "application/xml; charset=utf-8", String acceptType = "application/xml") {
+    public Response<HttpResponseDecorator> getResponseUsingMethodPUT(String url,
+                                                                     String data,
+                                                                     String apiKey,
+                                                                     String apiVersion,
+                                                                     String contentType = "application/xml; charset=utf-8",
+                                                                     String acceptType = "application/xml") {
 
         Response<HttpResponseDecorator> httpResponse = new Response<HttpResponseDecorator>()
         String authenticationDigest = apiKey.bytes.encodeBase64().toString()
 
         try {
             new RESTClient(url).with {
-                httpResponse.entity = put(body: data, contentType: contentType, headers: ["Accept": acceptType, "Authorization": authenticationDigest])
+                httpResponse.entity = put(
+                        body: data,
+                        contentType: contentType,
+                        headers: [
+                                "Accept": acceptType,
+                                "Authorization": "Basic ${authenticationDigest}",
+                                "Content-Type": contentType,
+                                "X-Api-Version": apiVersion
+                        ]
+                )
                 httpResponse.message = "Responce Generated via URL: ${url}"
                 httpResponse.status = httpResponse.entity.status
             }
@@ -49,14 +77,21 @@ class RESTService {
         return httpResponse
     }
 
-    public Response<HttpResponseDecorator> getResponseUsingMethodDELETE(String url, String apiKey, String acceptType = "application/xml") {
+    public Response<HttpResponseDecorator> getResponseUsingMethodDELETE(String url,
+                                                                        String apiKey,
+                                                                        String apiVersion,
+                                                                        String acceptType = "application/xml") {
 
         Response<HttpResponseDecorator> httpResponse = new Response<HttpResponseDecorator>()
         String authenticationDigest = apiKey.bytes.encodeBase64().toString()
 
         try {
             new RESTClient(url).with {
-                httpResponse.entity = delete(headers: ["Accept": acceptType, "Authorization": authenticationDigest])
+                httpResponse.entity = delete(headers: [
+                        "Accept": acceptType,
+                        "Authorization": "Basic ${authenticationDigest}",
+                        "X-Api-Version": apiVersion
+                ])
                 httpResponse.message = "Responce Generated via URL: ${url}"
                 httpResponse.status = httpResponse.entity.status
             }
@@ -69,14 +104,23 @@ class RESTService {
         return httpResponse
     }
 
-    public Response<HttpResponseDecorator> getResponseUsingMethodGET(String url, String apiKey, String contentType = "application/xml; charset=utf-8", String acceptType = "application/xml") {
+    public Response<HttpResponseDecorator> getResponseUsingMethodGET(String url,
+                                                                     String apiKey,
+                                                                     String apiVersion,
+                                                                     String contentType = "application/xml; charset=utf-8",
+                                                                     String acceptType = "application/xml") {
 
         Response<HttpResponseDecorator> httpResponse = new Response<HttpResponseDecorator>()
         String authenticationDigest = apiKey.bytes.encodeBase64().toString()
 
         try {
             new RESTClient(url).with {
-                httpResponse.entity = get(headers: ["Accept": acceptType, "Authorization": authenticationDigest])
+                httpResponse.entity = get(headers: [
+                        "Accept": acceptType,
+                        "Authorization": "Basic ${authenticationDigest}",
+                        "Content-Type": contentType,
+                        "X-Api-Version": apiVersion
+                ])
                 httpResponse.message = "Responce Generated via URL: ${url}"
                 httpResponse.status = httpResponse.entity.status
             }
